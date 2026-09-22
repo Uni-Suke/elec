0a71902608351c69fcc2c8e8f39e3062ced403b6
Uni <uni@unimac.local>
2026-09-22 22:12:36 +0900

    elec

--- src/pages/search-index.json.ts
+++ src/pages/search-index.json.ts
@@ -1,2 +1,10 @@
-import {getCollection} from 'astro:content';
-export async function GET(){const lessons=await getCollection('lessons');return new Response(JSON.stringify(lessons.map(e=>({id:e.id,...e.data,body:e.body||''}))),{headers:{'Content-Type':'application/json; charset=utf-8'}});}
+import { getCollection } from "astro:content";
+export async function GET() {
+  const lessons = await getCollection("lessons");
+  return new Response(
+    JSON.stringify(
+      lessons.map((e) => ({ id: e.id, ...e.data, body: e.body || "" })),
+    ),
+    { headers: { "Content-Type": "application/json; charset=utf-8" } },
+  );
+}
